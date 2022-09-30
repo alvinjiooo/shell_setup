715266d1158fb121ba32aa5a193af69939974f93
Alvin Ji <111466895+alvinjiooo@users.noreply.github.com>
2022-09-30 10:16:01 -0700

    Create alvin_cmd.sh

---
+++ alvin_cmd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Put all my custom command here
+
+function hey(){
+  echo "Test Success!"
+}
+
+function wpt(){
+  third_party/blink/tools/run_web_tests.py -t Default "$1"
+}
+
+function build(){
+  autoninja -C out/Default "$1"
+}
+
+function format(){
+ if [ -z "$1" ]
+ then git cl format "$1"
+ else git cl format
+ fi
+}
+
+function upload(){
+  git cl upload -T && git cl try
+}
+
+function sync(){
+  git pull && gclient sync -D && build
+}
